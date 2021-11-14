afa19a0472e96f94a475211ee115d2329d755417
uligi <59987716+uligi@users.noreply.github.com>
2021-11-13 18:30:19 -0600

    commit

--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -3,21 +3,37 @@
 <?php
 // $id = isset($_GET['id']) ? $_GET['id'] : '1';
 $id = $_GET['id'] ?? '1'; // PHP > 7.0
+
+$subject = find_subject_by_id($id);
+
 ?>
 
-<?php $page_title = 'Show Page'; ?>
+<?php $page_title = 'Show Subject'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
 
-  <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
+  <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
 
-  <div class="page show">
+  <div class="subject show">
 
-    Page ID: <?php echo h($id); ?>
+    <h1>Subject: <?php echo h($subject['menu_name']); ?></h1>
+
+    <div class="attributes">
+      <dl>
+        <dt>Menu Name</dt>
+        <dd><?php echo h($subject['menu_name']); ?></dd>
+      </dl>
+      <dl>
+        <dt>Position</dt>
+        <dd><?php echo h($subject['position']); ?></dd>
+      </dl>
+      <dl>
+        <dt>Visible</dt>
+        <dd><?php echo $subject['visible'] == '1' ? 'true' : 'false'; ?></dd>
+      </dl>
+    </div>
 
   </div>
 
 </div>
-
-<?php include(SHARED_PATH . '/staff_footer.php'); ?>
