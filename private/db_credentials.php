31bd17ef10b94b79113a5e1c57d076d30e8de4b9
uligi <59987716+uligi@users.noreply.github.com>
2021-11-13 21:41:57 -0600

    commit

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_SERVER',"Localhost");
-    define('DB_USER',"root");
-    define('DB_PASS',"Babascal13");
+    define('DB_USER',"admin");
+    define('DB_PASS',"bolsa1234");
     define('DB_NAME',"travel");
 
 ?>
