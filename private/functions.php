ff03b3b59b82a1a2100a20f64e8f6eb980ef605b
uligi <59987716+uligi@users.noreply.github.com>
2021-11-16 16:45:43 -0600

    commit

--- private/functions.php
+++ private/functions.php
@@ -1,47 +1,47 @@
-<?php 
-    function url_for($script_path) {
-        // add the leading '/' if not present
-        if($script_path[0] != '/') {
-          $script_path = "/" . $script_path;
-        }
-        return WWW_ROOT . $script_path;
-      }
+<?php
 
-      function u($string=""){
-          return urlencode($string);
-      }
+function url_for($script_path) {
+  // add the leading '/' if not present
+  if($script_path[0] != '/') {
+    $script_path = "/" . $script_path;
+  }
+  return WWW_ROOT . $script_path;
+}
 
-      function raw_u($string=""){
-          return rawurlencode($string);
-      }
+function u($string="") {
+  return urlencode($string);
+}
 
-      function h($string=""){
-          return htmlspecialchars($string);
-      }
+function raw_u($string="") {
+  return rawurlencode($string);
+}
 
-      function error_404(){
+function h($string="") {
+  return htmlspecialchars($string);
+}
 
-          header($_SERVER["SERVER_PROTOCOL"] . "404 NOT FOUND");
-          exit;
+function error_404() {
+  header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+  exit();
+}
 
-      }
+function error_500() {
+  header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
+  exit();
+}
 
-      function error_500(){
-        header($_SERVER["SERVER_PROTOCOL"] . "500 INTERNAL SERVER ERROR");
-        exit;
-          
-      }
-      function redirect_to($location){
-        header("Location: " . $location);
-        exit;
-      }
+function redirect_to($location) {
+  header("Location: " . $location);
+  exit;
+}
 
-      function is_post_request() {
-        return $_SERVER['REQUEST_METHOD'] == 'POST';
-      }
-      
-      function is_get_request() {
-        return $_SERVER['REQUEST_METHOD'] == 'GET';
-      }
-      
+function is_post_request() {
+  return $_SERVER['REQUEST_METHOD'] == 'POST';
+}
+
+function is_get_request() {
+  return $_SERVER['REQUEST_METHOD'] == 'GET';
+}
+
+
 ?>
