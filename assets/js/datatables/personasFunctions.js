c7b8fd1ac380a5a4098c10f9a5898d3d006e1f6b
uligi <59987716+uligi@users.noreply.github.com>
2021-11-28 14:06:38 -0600

    commit

---
+++ assets/js/datatables/personasFunctions.js
@@ -0,0 +1,282 @@
+//*****************************************************************
+//Inyección de eventos en el HTML
+//*****************************************************************
+
+$(function () { //para la creación de los controles
+    //agrega los eventos las capas necesarias
+    $("#enviar").click(function () {
+        addOrUpdatePersonas();
+    });
+    //agrega los eventos las capas necesarias
+    $("#cancelar").click(function () {
+        cancelAction();
+    });    //agrega los eventos las capas necesarias
+
+    $("#btMostarForm").click(function () {
+        //muestra el fomurlaior
+        clearFormPersonas();
+        $("#typeAction").val("add_personas");
+        $("#myModalFormulario").modal();
+    });
+    
+    
+    
+});
+
+//*********************************************************************
+//cuando el documento esta cargado se procede a cargar la información
+//*********************************************************************
+
+$(document).ready(function () {
+    cargarTablas();
+    
+});
+
+//*********************************************************************
+//Agregar o modificar la información
+//*********************************************************************
+
+function addOrUpdatePersonas() {
+    //Se envia la información por ajax
+    if (validar()) {
+        $.ajax({
+            url: 'Backend/controller/personasController.php',
+            data: {
+                action:                          $("#typeAction").val(),
+                Cedula:                          $("#txtCedula").val(),
+                Nombre:                          $("#txtNombre").val(),
+                Apellido1:                       $("#txtApellido1").val(),
+                Apellido2:                       $("#txtApellido2").val(),
+                Telefono:                        $("#txtTelefono").val(),
+                Correo_Electronico:              $("#txtCorreo").val(),
+                Tipo_Usuario:                    $("#txtTipousuario").val()
+            },
+            error: function () { //si existe un error en la respuesta del ajax
+                swal("Error", "Se presento un error al enviar la informacion", "error");
+            },
+            success: function (data) { //si todo esta correcto en la respuesta del ajax, la respuesta queda en el data
+                var messageComplete = data.trim();
+                var responseText = messageComplete.substring(2);
+                var typeOfMessage = messageComplete.substring(0, 2);
+                if (typeOfMessage === "M~") { //si todo esta corecto
+                    swal("Confirmacion", responseText, "success");
+                    clearFormPersonas();
+                    $("#dt_personas").DataTable().ajax.reload();
+                } else {//existe un error
+                    swal("Error", responseText, "error");
+                }
+            },
+            type: 'POST'
+        });
+    }else{
+        swal("Error de validación", "Los datos del formulario no fueron digitados, por favor verificar", "error");
+    }
+}
+
+//*****************************************************************
+//*****************************************************************
+function validar() {
+    var validacion = true;
+
+    
+    //valida cada uno de los campos del formulario
+    //Nota: Solo si fueron digitados
+    if ($("#txtCedula").val() === "") {
+        validacion = false;
+    }
+
+    if ($("#txtNombre").val() === "") {
+        validacion = false;
+    }
+
+    if ($("#txtApellido1").val() === "") {
+        validacion = false;
+    }
+
+    if ($("#txtApellido2").val() === "") {
+        validacion = false;
+    }
+
+    if ($("#txtTelefono").val() === "") {
+        validacion = false;
+    }
+
+    if ($("#txtCorreo").val() === "") {
+        validacion = false;
+    }
+
+    if ($("#txtTipousuario").val() === "") {
+        validacion = false;
+    }
+
+
+    return validacion;
+}
+
+//*****************************************************************
+//*****************************************************************
+
+function clearFormPersonas() {
+    $('#formPersonas').trigger("reset");
+}
+
+//*****************************************************************
+//*****************************************************************
+
+function cancelAction() {
+    //clean all fields of the form
+    clearFormPersonas();
+    $("#typeAction").val("add_personas");
+    $("#myModalFormulario").modal("hide");
+}
+
+
+
+//*****************************************************************
+//*****************************************************************
+
+function showPersonasByID(Cedula) {
+    //Se envia la información por ajax
+    $.ajax({
+        url: 'Backend/controller/personasController.php',
+        data: {
+            action: "show_personas",
+            Cedula: Cedula
+        },
+        error: function () { //si existe un error en la respuesta del ajax
+            swal("Error", "Se presento un error al consultar la informacion", "error");
+        },
+        success: function (data) { //si todo esta correcto en la respuesta del ajax, la respuesta queda en el data
+            var objPersonasJSon = JSON.parse(data);
+            $("#txtCedula").val(objPersonasJSon.Cedula);
+            $("#txtNombre").val(objPersonasJSon.Nombre);
+            $("#txtApellido1").val(objPersonasJSon.Apellido1);
+            $("#txtApellido2").val(objPersonasJSon.Apellido2);
+            $("#txtTelefono").val(objPersonasJSon.Telefono);
+            $("#txtCorreo").val(objPersonasJSon.Correo_Electronico);
+            $("#txtTipousuario").val(objPersonasJSon.Tipo_Usuario);
+            $("#typeAction").val("update_personas");
+            
+            swal("Confirmacion", "Los datos de la persona fueron cargados correctamente", "success");
+        },
+        type: 'POST'
+    });
+}
+
+//*****************************************************************
+//*****************************************************************
+
+function deletePersonasByID(Cedula) {
+    //Se envia la información por ajax
+    $.ajax({
+        url: 'Backend/controller/personasController.php',
+        data: {
+            action: "delete_personas",
+            Cedula: Cedula
+        },
+        error: function () { //si existe un error en la respuesta del ajax
+            swal("Error", "Se presento un error al eliminar la informacion", "error");
+        },
+        success: function (data) { //si todo esta correcto en la respuesta del ajax, la respuesta queda en el data
+            var responseText = data.trim().substring(2);
+            var typeOfMessage = data.trim().substring(0, 2);
+            if (typeOfMessage === "M~") { //si todo esta corecto
+                swal("Confirmacion", responseText, "success");
+                clearFormPersonas();
+                $("#dt_personas").DataTable().ajax.reload();
+            } else {//existe un error
+                swal("Error", responseText, "error");
+            }
+        },
+        type: 'POST'
+    });
+}
+
+
+
+
+//*******************************************************************************
+//Metodo para cargar las tablas
+//*******************************************************************************
+
+
+function cargarTablas() {
+
+
+
+    var dataTablePersonas_const = function () {
+        if ($("#dt_personas").length) {
+            $("#dt_personas").DataTable({
+                dom: "Bfrtip",
+                bFilter: true,
+                ordering: true,
+                buttons: [
+                    {
+                        extend: "copy",
+                        className: "btn-sm",
+                        text: "Copiar"
+                    },
+                    {
+                        extend: "csv",
+                        className: "btn-sm",
+                        text: "Exportar a CSV"
+                    },
+                    {
+                        extend: "print",
+                        className: "btn-sm",
+                        text: "Imprimir"
+                    }
+
+                ],
+                "columnDefs": [
+                    {
+                        targets: 6,
+                        className: "dt-center",
+                        render: function (data, type, row, meta) {
+                            var botones = '<button type="button" class="btn btn-default btn-xs" aria-label="Left Align" onclick="showPersonasByID(\''+row[0]+'\');">Cargar</button> ';
+                            botones += '<button type="button" class="btn btn-default btn-xs" aria-label="Left Align" onclick="deletePersonasByID(\''+row[0]+'\');">Eliminar</button>';
+                            return botones;
+                        }
+                    }
+
+                ],
+                pageLength: 5,
+                language: dt_lenguaje_espanol,
+                ajax: {
+                    url: 'Backend/controller/personasController.php',
+                    type: "POST",
+                    data: function (d) {
+                        return $.extend({}, d, {
+                            action: "showAll_personas"
+                        });
+                    }
+                },
+                drawCallback: function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+                    $('#dt_personas').DataTable().columns.adjust().responsive.recalc();
+                }
+            });
+        }
+    };
+
+
+
+    TableManageButtons = function () {
+        "use strict";
+        return {
+            init: function () {
+                dataTablePersonas_const();
+                $(".dataTables_filter input").addClass("form-control input-rounded ml-sm");
+            }
+        };
+    }();
+
+    TableManageButtons.init();
+}
+
+//*******************************************************************************
+//evento que reajusta la tabla en el tamaño de la pantall
+//*******************************************************************************
+
+window.onresize = function () {
+    $('#dt_personas').DataTable().columns.adjust().responsive.recalc();
+};
---
+++ backend/admin/admin_user.php
@@ -0,0 +1,110 @@
+<?php  
+require_once('admin_header.php');
+
+?>
+    <body>
+
+        <!-- ********************************************************** -->
+        <!-- ********************************************************** -->
+        <!-- Modal del BootsTrap para mostrar mensajes                  -->
+        <!-- ********************************************************** -->
+        <!-- ********************************************************** -->
+        <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog modal-sm">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title" id="myModalTitle">Modal Header</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body" id="myModalMessage">
+                        <p>This is a small modal.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="page-header">
+                <h1><h1><Nombre Sistema><small> Mantenimiento de Personas</small><img src="img/logo/logo.png" align="right"/></h1></h1>
+            </div>  
+            
+            <!-- ********************************************************** -->
+            <!-- CONTENIDO DE LA PAGINA                                     -->
+            <!-- ********************************************************** -->
+            
+            <div class="row">
+                <div class="col-md-12">
+                    <form role="form" onsubmit="return false;" id="formPersonas" action="../backend/controller/personasController.php">
+                        <div class="row">
+                            <!-- ******************************************************** -->
+                            <!-- Campos de formulario      -->
+                            <!-- ******************************************************** -->
+                            <div class="col-md-12">
+
+                                <div class="form-group" id="groupPK_cedula">
+                                    <label for="txtPK_cedula">Cédula</label>
+                                    <input type="text" class="form-control" id="txtPK_cedula"  placeholder="Cédula">
+                                </div>
+                                <div class="form-group" id="groupnombre">
+                                    <label for="txtnombre">Nombre</label>
+                                    <input type="text" class="form-control" id="txtnombre"  placeholder="Nombre">
+                                </div>
+                                <div class="form-group" id="groupapellido1">
+                                    <label for="txtapellido1">Primer Apellido</label>
+                                    <input type="text" class="form-control" id="txtapellido1"  placeholder="Primer Apellido">
+                                </div>
+                                <div class="form-group" id="groupapellido2">
+                                    <label for="txtapellido2">Segundo Apellido</label>
+                                    <input type="text" class="form-control" id="txtapellido2"  placeholder="Segundo Apellido">
+                                </div>
+                                <div class="form-group" id="groupfecNacimiento">
+                                    <label for="txtfecNacimiento">Fecha Nacimiento</label>
+                                    <input type="text" class="form-control" id="txtfecNacimiento"  placeholder="Fec. Nacimiento">
+                                </div>
+                                <div class="form-group" id="groupsexo">
+                                    <label for="txtsexo">Sexo</label>
+                                    <input type="text" class="form-control" id="txtsexo"  placeholder="Sexo">
+                                </div>
+                                <div class="form-group" id="groupobservaciones">
+                                    <label for="txtobservaciones">Observaciones</label>
+                                    <input type="text" class="form-control" id="txtobservaciones"  placeholder="Observaciones">
+                                </div>
+                                <div class="form-group">
+                                    <input type="hidden" id="typeAction" value="add_personas" />
+                                    <button type="submit" class="btn btn-primary" id="enviar">Guardar</button>
+                                    <button type="reset" class="btn btn-danger" id="cancelar">Cancelar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <br>
+            <h3>Tabla con informacion de personas</h3>
+
+            <br><br>
+            <div class="row">
+                <div class="col-md-12">
+                    <table id="dt_personas"  class="table  table-hover dt-responsive nowrap" cellspacing="0" width="100%">
+                        <thead>
+                            <tr>
+                                <th>CEDULA</th>
+                                <th>NOMBRE</th>
+                                <th>APELLIDO1</th>
+                                <th>APELLIDO2</th>
+                                <th>FEC. NACIMIENTO</th>
+                                <th>SEXO</th>
+                                <th>ACCION</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+
+            <br><br><br><br>
+            <!-- ********************************************************** -->	
+            <!-- FIN CONTENIDO DE LA PAGINA                                 -->
+            <!-- ********************************************************** -->	
+        </div>
+    </body>
+</html>
